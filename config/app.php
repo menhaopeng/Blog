d4d8959b82997d98b45a293cf5754e46dc6dac2f
menhaopeng <menhaopeng>
2017-10-17 22:02:01 +0800

    【版本更新】Laravel版本更新到5.5

--- config/app.php
+++ config/app.php
@@ -178,7 +178,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Barryvdh\Debugbar\ServiceProvider::class,
-        Nwidart\DbExporter\DbExportHandlerServiceProvider::class
+//        Nwidart\DbExporter\DbExportHandlerServiceProvider::class
     ],
 
     /*
